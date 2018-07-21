d9d1ba94f3f0415a2cc71740140592cf903abfac
Laurids <me@lauridskern.com>
2018-07-21 16:04:58 +0200

    Fixed like.js

--- like.js
+++ like.js
@@ -1,46 +1,44 @@
 /**
  * Module Dependencies
  */
-const Nightmare = require('nightmare'),
-      nightmare = Nightmare({ show: false }),
-      config    = require('./config')
+const Nightmare = require("nightmare"),
+  nightmare = Nightmare({ show: false }),
+  config = require("./config");
 
 /**
  * Like Interval
  */
 function like() {
+  Nightmare({ show: false })
+    .goto("https://dribbble.com/session/new")
+    .type("#login", `${config.username}`)
+    .type("#password", `${config.password}`)
+    .wait(2500)
+    .click('input[type="submit"]')
+    .wait(2500)
+    .goto("https://dribbble.com/shots?sort=recent")
+    .wait(2500)
+    .click(".dribbble-over")
+    .wait(2500)
+    .click(":not(.current-user-likes).like-shot")
+    .wait(Math.floor(Math.random() * 2500) + 1)
+    .evaluate(function() {
+      return `Liked ${window.location.href}`;
+    })
+    .end()
+    .then(function(result) {
+      console.log(result);
+      console.log("Waiting 144000 milliseconds...");
+    })
+    .catch(function(error) {
+      console.error("Skipping...");
+      console.log("Waiting 144000 milliseconds...");
+    });
 
-    nightmare
-        .goto('https://dribbble.com/session/new')
-        .type('#login', `${config.username}`)
-        .type('#password', `${config.password}`)
-        .wait(2500)
-        .click('input[type="submit"]')
-        .wait(2500)
-        .goto('https://dribbble.com/shots?sort=recent')
-        .wait(2500)
-        .click('.dribbble-over')
-        .wait(2500)
-        .click(':not(.current-user-likes).like-shot')
-        .wait(Math.floor(Math.random() * 2500) + 1)
-        .evaluate(function () {
-            return `Liked ${window.location.href}`
-        })
-        .end()
-        .then(function (result) {
-            console.log(result)
-            console.log('Waiting 144000 milliseconds...')
-        })
-        .catch(function (error) {
-            console.error('Skipping...')
-            console.log('Waiting 144000 milliseconds...')
-        })
-
-        setTimeout(like, 144000)
-
+  setTimeout(like, 144000);
 }
 
 /**
  * Start the Cycle
  */
-like()
+like();
